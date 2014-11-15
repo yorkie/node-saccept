e02a320520761a952eebf0642e55e83ead9f5f25
Yazhong Liu <yorkiefixer@gmail.com>
2014-11-15 23:39:55 +0800

    complete

---
+++ README.md
@@ -0,0 +1,21 @@
+
+node-saccept
+====================
+Semantic HTTP accept parser
+
+### What's Semantic HTTP Accept?
+
+```
+application/vnd.github.v3+json
+```
+
+Insipred by [Github API documentation](https://developer.github.com/v3/).
+Coming Soon...
+
+### Installation
+
+```sh
+$ npm install --save saccept
+```
+
+### 
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 
+var semver = require('semver');
+
+var reg = /(.*)\/vnd\..*\.?v(.*)\+(.*)/;
 function valid (raw) {
   return raw.trim().split(';')
     .map(function (accept) {
-      var obj = accept.split('.').pop();
-      if (obj.split) {
-        obj = obj.split('+');
+      var m = accept.match(reg);
+      if (m && (m = m.slice(1))) {
         return {
-          version: obj[0],
-          type: obj[1]
+          mime: m[0] + m[2],
+          version: m[1]
         };
       } else {
         return null;
@@ -15,7 +17,19 @@ function valid (raw) {
     })
     .filter(function (obj) {
       return obj;
-    });
+    })[0];
 }
 
-module.exports = valid;
+function accept (raw, res, next, isStrong, ver, mime) {
+  var obj = valid(raw);
+  if (!obj.version)
+    return isStrong ? res.status(400).end() : next();
+  if (semver.satisfies(obj.version, ver))
+    if (!mime || mime === obj.mime)
+      return next();
+  else
+    return res.status(400).end();
+}
+
+exports.valid = valid;
+exports.accept = accept;
